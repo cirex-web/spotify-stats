26c7dcc29a60cf50a43bf2f31f065655b1e23342
topshenyi-web <erxu@ctemc.org>
2024-03-10 22:30:44 -0400

    why is this recording the laggiest thing in the world

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,34 @@ const spotifyAPI = new SpotifyAPI(
 );
 await spotifyAPI.init();
 
+// shamelessly taken from stack overflow
+function record(canvas: HTMLCanvasElement) {
+  const recordedChunks: Blob[] = [];
+  var stream = canvas.captureStream(60 /*fps*/);
+  const mediaRecorder = new MediaRecorder(stream, {
+    mimeType: "video/webm; codecs=vp9",
+  });
+
+  //ondataavailable will fire in interval of `time || 4000 ms`
+  mediaRecorder.start();
+
+  mediaRecorder.ondataavailable = function (event) {
+    recordedChunks.push(event.data);
+  };
+
+  mediaRecorder.onstop = function (event) {
+    const url = URL.createObjectURL(
+      new Blob(recordedChunks, { type: "video/webm" })
+    );
+    const anchor = document.createElement("a");
+    anchor.href = url;
+    anchor.download = "video.webm";
+    anchor.click();
+    window.URL.revokeObjectURL(url);
+  };
+  setTimeout(() => mediaRecorder.stop(), 10000);
+}
+
 const canvas = document.getElementsByTagName(
   "canvas"
 )[0] as HTMLCanvasElement | null;
@@ -29,5 +57,6 @@ console.log(dayData, windowData, idToSong);
 // Ok so we pass in the entire Frame (map) for every date, cuz otherwise the interpolated data would be kind of wrong if we truncated and/or sorted the data beforehand.
 const barAnimator = new BarAnimator(windowData, idToSong, canvas);
 barAnimator.startLoop(new Date("1/1/23"));
+record(canvas);
 
 // okay wait maybe we should only pass in the final sorted data with all relevant properties
